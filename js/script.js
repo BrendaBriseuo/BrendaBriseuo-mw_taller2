4b0fc32a93061302d838fdddc1cc49803ccc05c0
BrendaBriseuo <68075185+BrendaBriseuo@users.noreply.github.com>
2020-10-04 21:12:20 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 
 alert("¡Bienvenidas mujeres emprendedoras!");
 confirm(" ¿Deseas saber acerca de autonomía económica de las mujeres?");
+var nombre=prompt("Ingresa tu nombre");
+document.write("<p>");
+document.write("Hola "+nombre);
+document.write("<p>");
